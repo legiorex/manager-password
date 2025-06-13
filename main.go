b3a145e13946d240618563d04b2ac18c7330a6f0
Konstantin Batiushev <batyushev@gmail.com>
2025-06-14 06:27:08 +0700

    part 9.5

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ type account struct {
 }
 
 func main() {
-    login := promptData("Введите логин: ")
-    password := promptData("Введите пароль: ")
-    url := promptData("Введите URL: ")
+    login := promptData("Введите логин")
+    password := promptData("Введите пароль")
+    url := promptData("Введите URL")
     
     accountNew := account{
         login: login,
@@ -19,13 +19,19 @@ func main() {
         url: url,
     }
     
-    fmt.Println(accountNew)
+    printAccount(&accountNew)
 
 }
 
 func promptData(message string) string {
-    fmt.Println(message)
+    fmt.Print(message + ": ")
     var result string
     fmt.Scanln(&result)
     return result
+}
+
+func printAccount(acc *account) {
+    fmt.Println("Логин: ", acc.login)
+    fmt.Println("Пароль: ", acc.password)
+    fmt.Println("URL: ", acc.url)
 }
