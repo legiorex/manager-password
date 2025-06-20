4c4ac16f415bfbf905285efb2a7d835f6b2e8408
Konstantin Batiushev <batyushev@gmail.com>
2025-06-20 09:26:28 +0700

    Refactor account methods to use pointer receiver and update main function to generate password if not provided

--- main.go
+++ main.go
@@ -12,37 +12,13 @@ type account struct {
 	url      string
 }
 
-func (acc account) printAccount() {
+func (acc *account) printAccount() {
 	fmt.Println("Логин: ", acc.login)
 	fmt.Println("Пароль: ", acc.password)
 	fmt.Println("URL: ", acc.url)
 }
 
-func main() {
-	generatePassword(25)
-
-	login := promptData("Введите логин")
-	password := promptData("Введите пароль")
-	url := promptData("Введите URL")
-
-	accountNew := account{
-		login:    login,
-		password: password,
-		url:      url,
-	}
-
-	accountNew.printAccount()
-
-}
-
-func promptData(message string) string {
-	fmt.Print(message + ": ")
-	var result string
-	fmt.Scanln(&result)
-	return result
-}
-
-func generatePassword(n int) {
+func (acc *account) generatePassword(n int) {
 
 	// создаем 32-байтное семя для инициализации генератора
 	seed := [32]byte{'s', 'o', 'm', 'e', 'k', 'e', 'y', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'}
@@ -67,6 +43,32 @@ func generatePassword(n int) {
 
 	resultPassword := strBuilder.String()
 
-	println(resultPassword)
+	acc.password = resultPassword
+}
 
+func main() {
+
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	accountNew := account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+
+	if accountNew.password == "" {
+		accountNew.generatePassword(12)
+	}
+
+	accountNew.printAccount()
+
+}
+
+func promptData(message string) string {
+	fmt.Print(message + ": ")
+	var result string
+	fmt.Scanln(&result)
+	return result
 }
