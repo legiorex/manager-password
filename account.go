b248d883d0c9dc8af1f51c476e73eb8835042172
Konstantin Batiushev <batyushev@gmail.com>
2025-06-21 09:58:15 +0700

    Remove unused account struct and related methods, simplifying account creation logic in main function.

---
+++ account.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+type accountWithTimeStamp struct {
+	createdAt time.Time
+	updatedAt time.Time
+	account
+}
+
+func (acc *account) printAccount() {
+	fmt.Println("Логин: ", acc.login)
+	fmt.Println("Пароль: ", acc.password)
+	fmt.Println("URL: ", acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+
+	// создаем 32-байтное семя для инициализации генератора
+	seed := [32]byte{'s', 'o', 'm', 'e', 'k', 'e', 'y', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f'}
+
+	// инициализируем новый генератор ChaCha8
+	rand.NewChaCha8(seed)
+
+	// start 65
+	// finish 90
+
+	min := 65
+	max := 90
+
+	var strBuilder strings.Builder
+
+	for range n {
+		genRune := rand.IntN(max-min+1) + min
+
+		strBuilder.WriteRune(rune(genRune))
+
+	}
+
+	resultPassword := strBuilder.String()
+
+	acc.password = resultPassword
+}
+
+// func newAccount(login, password, urlUser string) (*account, error) {
+
+// 	if login == "" {
+// 		return nil, errors.New("INVALID_LOGIN")
+// 	}
+
+// 	_, errUrl := url.ParseRequestURI(urlUser)
+
+// 	if errUrl != nil {
+// 		return nil, errors.New("INVALID_URL")
+// 	}
+
+// 	acc := &account{
+// 		login:    login,
+// 		password: password,
+// 		url:      urlUser,
+// 	}
+
+// 	if acc.password == "" {
+// 		acc.generatePassword(12)
+// 	}
+
+// 	return acc, nil
+// }
+
+func newAccountWithTimeStamp(login, password, urlUser string) (*accountWithTimeStamp, error) {
+
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+
+	_, errUrl := url.ParseRequestURI(urlUser)
+
+	if errUrl != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	acc := &accountWithTimeStamp{
+
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		account: account{
+			login:    login,
+			password: password,
+			url:      urlUser,
+		},
+	}
+
+	if acc.password == "" {
+		acc.generatePassword(12)
+	}
+
+	return acc, nil
+}
